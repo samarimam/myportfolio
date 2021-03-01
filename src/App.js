cb02d37e44059d029210c4c7d5a1daf4902f114a
samarimam <samarimam78@gmail.com>
2021-03-01 16:42:01 +0530

    added carousal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from "react";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import MyTitleMessage from "./components/title-message/title-message.component";
 import MyNavbar from "./components/my-navbar/my-navbar.component";
 
 const App = () => {
   return (
     <div className="App">
      <MyNavbar />
+     <MyTitleMessage />
+     <MyCarousal />
     </div>
   );
 };
